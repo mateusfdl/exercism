777589eb5e88ad942972cb8541853c4d1c65961d
Matheus Furtado <matheus.limastack@outlook.com>
2020-03-18 16:38:12 -0300

    High score exercism

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -5,4 +5,28 @@ Write your code for the 'High Scores' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/high-scores` directory.
 =end
+class HighScores
+  attr_accessor :scores
 
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+
+    scores.last
+  end
+
+  def personal_best
+
+    scores.max
+  end
+
+  def personal_top_three
+    scores.max(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
