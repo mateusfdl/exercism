442e26e17a475e6aadef819c28c7d367f2f7ecfe
Matheus Furtado <matheus.limastack@outlook.com>
2020-05-29 16:24:17 -0300

    Submit Ruby matrix exercism

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,8 +1,9 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+class Matrix
+  def initialize(string)
+    @rows = string.split("\n").map {|x| x.split(' ').map(&:to_i)}
+    @columns = rows.transpose
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+  attr_reader :rows, :columns
+end
 
