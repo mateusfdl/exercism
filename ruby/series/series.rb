d923b4d2cd768533b307a96451d8166b77d1be81
Matheus Lima <matheus.limastack@outlook.com>
2020-06-28 03:43:35 -0300

    Add series exercism

---
+++ ruby/series/series.rb
@@ -0,0 +1,22 @@
+class Series
+ private
+
+ attr_accessor :string
+
+  def initialize(string)
+		  @string = string.chars
+  end
+
+ public
+
+  def slices(lenght)
+	unless lenght <= string.length
+
+		raise ArgumentError, 'Slices length cannot be longer than series length'
+
+	end
+
+	@string.each_cons(lenght).map(&:join)
+  end
+
+end
