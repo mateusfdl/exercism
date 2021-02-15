0a753da40213eda027b40cf7430bb3d4f1f5f41d
Matheus Lima <matheus.limastack@outlook.com>
2021-02-15 05:34:39 -0300

    Removed comments from TwoFer exercism

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,11 +1,3 @@
-=begin
-Write your code for the 'Two Fer' exercise in this file. Make the tests in
-`two_fer_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/two-fer` directory.
-=end
-
 class TwoFer
 
     def self.two_fer(name = "you")
@@ -13,7 +5,3 @@ class TwoFer
         string = "One for #{name}, one for me."
     end
 end
-
-@class = TwoFer.two_fer("Bob")
-
-p @class
