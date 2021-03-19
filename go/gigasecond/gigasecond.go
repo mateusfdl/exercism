83673f0218695f57e9e4c93310c6a48774485284
Matheus Lima <matheus.limastack@outlook.com>
2021-03-19 16:34:54 -0300

    Completed Go gigasecond exercism

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,16 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package gigasecond should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// AddGigasecond should have a comment documenting it.
+func AddGigasecond(t time.Time) time.Time {
+	gigasecond := 1e9
+
+	return t.Add(time.Second * time.Duration(gigasecond))
+      }
