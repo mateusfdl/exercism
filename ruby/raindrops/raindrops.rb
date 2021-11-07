1673fc9769c4d3ef30dcafd76936e9c3d67fca98
Matheus Lima <matheus.limastack@outlook.com>
2021-11-06 21:07:42 -0300

    style: fix raindrops Ruby exercism

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,31 +1,29 @@
 class Raindrops
-    
-   RAINDROPS = {
-      3 => "Pling",
-      5 => "Plang",
-      7 => "Plong"
-    }.freeze
+  class << self
+    def convert(value)
+      new(value).sound
+    end
+  end
+
+  attr_reader :value
+  
+  RAINDROPS = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }.freeze
 
   def initialize(value)
     @value = value
   end
 
   def factors
-    RAINDROPS.map { _2 if @value % _1 == 0 }.compact.join
+    RAINDROPS.map { _2 if (@value % _1).zero? }.compact.join
   end
 
   def sound
     return value.to_s if factors.empty?
 
     factors
-  end
-
-  attr_reader :value
-
-  class << self
-
-    def convert(value)
-      new(value).sound
-    end
   end
 end
