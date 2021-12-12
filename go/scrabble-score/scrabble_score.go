37d8d934c1b59178c844fc08bd76a6df603e15e8
Matheus Lima <matheus.limastack@outlook.com>
2021-12-11 23:34:01 -0300

    Completed scrabble-score Golang exercism

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -6,43 +6,41 @@ import (
 )
 
 var (
-	onePointRegexp = regexp.MustCompile("^[a|e|i|o|u|l|n|r|s|t]")
-	twoPointsRegexp = regexp.MustCompile("^[d|g]")
-	treePointsRegexp = regexp.MustCompile("^[b|c|m|p]")
-	fourPointsRegexp = regexp.MustCompile("^[f|h|v|w|y]")
-	fivePointsRegexp = regexp.MustCompile("^[k]")
+	onePointRegexp    = regexp.MustCompile("^[a|e|i|o|u|l|n|r|s|t]")
+	twoPointsRegexp   = regexp.MustCompile("^[d|g]")
+	treePointsRegexp  = regexp.MustCompile("^[b|c|m|p]")
+	fourPointsRegexp  = regexp.MustCompile("^[f|h|v|w|y]")
+	fivePointsRegexp  = regexp.MustCompile("^[k]")
 	eightPointsRegexp = regexp.MustCompile("^[j|x]")
-	tenPointsRegexp = regexp.MustCompile("^[q|z]")
+	tenPointsRegexp   = regexp.MustCompile("^[q|z]")
 )
+
 func Score(word string) int {
 	points := 0
 
-	for _, value := range word {
+	for _, value := range strings.ToLower(word) {
 		points += pointsFor(string(value))
 	}
-	
+
 	return points
 }
 
-func pointsFor(str string) int{
-		switch {
-		case onePointRegexp.MatchString(str):
+func pointsFor(str string) int {
+	switch {
+	case onePointRegexp.MatchString(str):
 		return 1
-		case twoPointsRegexp.MatchString(str):
-			return 2
-		case treePointsRegexp.MatchString(str):
-			return 3
-		case fourPointsRegexp.MatchString(str):
-			return 4
-		case fivePointsRegexp.MatchString(str):
-			return 5
-		case eightPointsRegexp.MatchString(str):
-			return 8
-		case tenPointsRegexp.MatchString(str):
-			return 10
-		}
-		return 0
+	case twoPointsRegexp.MatchString(str):
+		return 2
+	case treePointsRegexp.MatchString(str):
+		return 3
+	case fourPointsRegexp.MatchString(str):
+		return 4
+	case fivePointsRegexp.MatchString(str):
+		return 5
+	case eightPointsRegexp.MatchString(str):
+		return 8
+	case tenPointsRegexp.MatchString(str):
+		return 10
+	}
+	return 0
 }
-
-
-
