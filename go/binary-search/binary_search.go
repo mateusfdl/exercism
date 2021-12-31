beb2ecfa2d8c77a815cc61c6d30f0d74a82b66e7
Matheus Lima <matheus.limastack@outlook.com>
2021-12-31 02:46:56 -0300

    Completed binary search Golang exercism

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,21 +1,23 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	return RecursiveBinarySearch(list, key)
+	return RecursiveBinarySearch(list, 0, len(list)-1, key)
 }
 
-func RecursiveBinarySearch(list []int, key int) int {
-	middle := len(list) / 2
+func RecursiveBinarySearch(list []int, listMinimumLenght int, listLenght int, key int) int {
+	middle := (listMinimumLenght + listLenght) / 2
 
-	if list[middle] == key {
-		return middle
-	}
-	if len(list) == 1 {
+	if listMinimumLenght > listLenght {
 		return -1
 	}
-	if list[middle] > key {
-		return RecursiveBinarySearch(list[:middle], key)
+
+	if key < list[middle] {
+		return RecursiveBinarySearch(list, listMinimumLenght, middle-1, key)
 	}
 
-	return RecursiveBinarySearch(list[middle:], key)
+	if key > list[middle] {
+		return RecursiveBinarySearch(list, middle+1, listLenght, key)
+	}
+
+	return middle
 }
