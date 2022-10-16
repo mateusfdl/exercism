bd356b2120a6cc0f3ac1412aa982719856242e69
Matheus Lima <matheus.limastack@outlook.com>
2022-10-16 01:07:58 -0300

    Completed AmusementPark Ruby exercism

---
+++ ruby/amusement-park/attendee.rb
@@ -0,0 +1,15 @@
+class Attendee
+  attr_accessor :height, :pass_id
+
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+end
