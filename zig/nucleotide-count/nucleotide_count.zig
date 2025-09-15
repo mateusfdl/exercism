9096fc9fb8eda9a6049d5d98873439347f407560
Matheus Lima <matheus.limastack@outlook.com>
2025-09-15 07:29:51 -0300

    completed Zig nucleotide-count

---
+++ zig/nucleotide-count/nucleotide_count.zig
@@ -0,0 +1,30 @@
+const std: type = @import("std");
+pub const NucleotideError = error{Invalid};
+
+pub const Counts = struct {
+    a: u32,
+    c: u32,
+    g: u32,
+    t: u32,
+};
+
+pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
+    var counts = Counts{
+        .a = 0,
+        .c = 0,
+        .g = 0,
+        .t = 0,
+    };
+
+    for (s) |n| {
+        switch (n) {
+            'A' => counts.a += 1,
+            'C' => counts.c += 1,
+            'G' => counts.g += 1,
+            'T' => counts.t += 1,
+            else => return NucleotideError.Invalid,
+        }
+    }
+
+    return counts;
+}
