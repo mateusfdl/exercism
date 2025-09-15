fd9ff20385d65e953d7f844ecb615ebd03060827
Matheus Lima <matheus.limastack@outlook.com>
2025-09-15 07:34:43 -0300

    refactor Zig nucleotide-count

--- zig/nucleotide-count/nucleotide_count.zig
+++ zig/nucleotide-count/nucleotide_count.zig
@@ -6,25 +6,30 @@ pub const Counts = struct {
     c: u32,
     g: u32,
     t: u32,
-};
 
-pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
-    var counts = Counts{
-        .a = 0,
-        .c = 0,
-        .g = 0,
-        .t = 0,
-    };
+    fn init() Counts {
+        return Counts{
+            .a = 0,
+            .c = 0,
+            .g = 0,
+            .t = 0,
+        };
+    }
 
-    for (s) |n| {
+    fn inc(self: *Counts, n: u8) NucleotideError!void {
         switch (n) {
-            'A' => counts.a += 1,
-            'C' => counts.c += 1,
-            'G' => counts.g += 1,
-            'T' => counts.t += 1,
+            'A' => self.a += 1,
+            'C' => self.c += 1,
+            'G' => self.g += 1,
+            'T' => self.t += 1,
             else => return NucleotideError.Invalid,
         }
     }
+};
+
+pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
+    var counts = Counts.init();
+    for (s) |n| try counts.inc(n);
 
     return counts;
 }
