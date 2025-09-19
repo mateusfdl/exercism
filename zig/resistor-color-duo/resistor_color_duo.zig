6ab6188ed07d5ce556ec1d53c681d7d30b5ed23a
Matheus Lima <matheus.limastack@outlook.com>
2025-09-19 15:11:39 -0300

    refactor Zig resist-color-duo

--- zig/resistor-color-duo/resistor_color_duo.zig
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -12,20 +12,5 @@ pub const ColorBand = enum(u4) {
 };
 
 pub fn colorCode(colors: [2]ColorBand) usize {
-    return concatInts(@intFromEnum(colors[0]), @intFromEnum(colors[1]));
-}
-
-fn concatInts(a: usize, b: usize) usize {
-    var pow10: usize = 1;
-    var tmp: usize = b;
-
-    if (tmp == 0) {
-        pow10 = 10;
-    } else {
-        while (tmp > 0) : (tmp /= 10) {
-            pow10 *= 10;
-        }
-    }
-
-    return a * pow10 + b;
+    return @intFromEnum(colors[0]) * 10 + @intFromEnum(colors[1]);
 }
